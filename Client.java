59c13ba79e08e106eb7a9444cc1dc5a41f3a75bf
Frederik Steenberg <75301197+Fevek05@users.noreply.github.com>
2023-09-07 15:39:55 +0200

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of class Client here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Client extends IpMachine
+{
+    /**
+     * Constructor for objects of class Client
+     */
+    
+    
+    public Client(Internet connectedToInternet)
+    //The client has to be be connected to an internet when created
+    {
+        super(connectedToInternet);
+    }
+    
+    @Override
+    public IpMessage receive(IpMessage message)
+    {
+        if (message.getRecipient() != this.getId()) 
+            return message;  //message er ikke til mig!
+        message.hasArrived(); //beskeden er kommet frem.
+        System.out.println("clienten " + this + " har modtaget beskeden:");
+        message.print();
+        System.out.println();
+        
+        if (message.getMessage() == "password")
+        {
+            getInternet().transmit(new IpMessage(this.getId(), message.getSender(), "1234"));
+        }
+        
+        //Metoden skal returnere en IpMessage, 
+        //men det giver ikke mening at sende noget videre,
+        //så returnerer null.
+        return null;
+    }
+    
+    public void startSession(String serverId)
+    {
+        getInternet().transmit(new IpMessage(this.getId(), serverId, "logon"));
+    }
+
+}
