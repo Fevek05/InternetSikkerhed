24ac7ea20fe91641f9de38dae2df7c7803a669df
Fevek05 <steenbergfrederik@gmail.com>
2023-09-08 09:35:07 +0200

    k

--- IpMessage.java
+++ IpMessage.java
@@ -2,38 +2,41 @@
 /**
  * IpMessage emulerer en besked sendt over internettet.
  * Du er velkommen til at lave så mange String-felter som du lyster
- * I  virkeligheden er det IpMessage en lang string og de forskellige felter svarer så til forskellige dele 
- * af den lange samlede String, men det er noget nemmere at arbejde med forskellige felter til forskellige formål
+ * I virkeligheden er det IpMessage en lang string og de forskellige felter
+ * svarer så til forskellige dele
+ * af den lange samlede String, men det er noget nemmere at arbejde med
+ * forskellige felter til forskellige formål
  * 
- * Husk blot, hvis du tilføjer flere felter til IpMessage,  så skal du også udskrive dem i print 
- * (ellers er det snyd for en Hacker kan se hele den lange String) 
+ * Husk blot, hvis du tilføjer flere felter til IpMessage, så skal du også
+ * udskrive dem i print
+ * (ellers er det snyd for en Hacker kan se hele den lange String)
  *
  * @author Kasper Astrup Eriksen
  * @version (v1)
  */
-public class IpMessage implements Cloneable //implements Cloneable er for at 
-//det er muligt at lave en kopi af en IpMessage med den indbygged .clone() method.
+public class IpMessage implements Cloneable // implements Cloneable er for at
+// det er muligt at lave en kopi af en IpMessage med den indbygged .clone()
+// method.
 {
     // instance variables - replace the example below with your own
-    private String from;  //Id på afsenderen af beskeden
-    private String to;    //Id på modtageren af beskeden
-    private String message;  //noget indholdstekst
+    private String from; // Id på afsenderen af beskeden
+    private String to; // Id på modtageren af beskeden
+    private String message; // noget indholdstekst
     // Husk at tilføje til print-metoden, hvis du tilføjer flere felter.
-    
-    private boolean arrived; //markerer beskeden er kommet frem. bruges vist ikke til så meget pt.
+
+    private boolean arrived; // markerer beskeden er kommet frem. bruges vist ikke til så meget pt.
 
     /**
      * Constructor for objects of class IpMessage
      */
     public IpMessage()
-    //Laver en tom død besked
+    // Laver en tom død besked
     {
         arrived = true;
-        //resten af variablerne er bare null.
+        // resten af variablerne er bare null.
     }
-    
-    public IpMessage(String from, String to, String message)
-    {
+
+    public IpMessage(String from, String to, String message) {
         // initialise instance variables
         this.from = from;
         this.to = to;
@@ -44,84 +47,75 @@ public class IpMessage implements Cloneable //implements Cloneable er for at
     /**
      * An example of a method - replace this comment with your own
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param y a sample parameter for a method
+     * @return the sum of x and y
      */
-    public String getRecipient()
-    {
+    public String getRecipient() {
         // put your code here
         return to;
     }
-    
-    public void setRecipient(String to)
-    {
-        this.to = to;    
+
+    public void setRecipient(String to) {
+        this.to = to;
     }
-    
-    public String getSender()
-    {
+
+    public String getSender() {
         // put your code here
         return from;
     }
-    
-    public void setSender(String from)
-    {
+
+    public void setSender(String from) {
         this.from = from;
     }
-    
-    public String getMessage()
-    {
+
+    public String getMessage() {
         // put your code here
         return message;
     }
-    
-    public void setMessage(String message)
-    {
+
+    public void setMessage(String message) {
         this.message = message;
     }
-    
-    /*public boolean isDeleted()
-    {
-        // put your code here
-        return !deleted;
-    }*/
-    
-    public boolean getArrived()
-    {
+
+    /*
+     * public boolean isDeleted()
+     * {
+     * // put your code here
+     * return !deleted;
+     * }
+     */
+
+    public boolean getArrived() {
         // put your code here
         return arrived;
     }
-    
-    public void hasArrived()
-    {
+
+    public void hasArrived() {
         arrived = true;
     }
-    
-    public void delete()
-    {
+
+    public void delete() {
         hasArrived();
         this.to = null;
         this.from = null;
         this.message = null;
     }
-    
+
     public void print() {
         System.out.println("IpMessage: " + this);
         System.out.println("From: " + getSender());
         System.out.println("To: " + getRecipient());
-        System.out.println("Message: " + getMessage()); 
+        System.out.println("Message: " + getMessage());
     }
-    
-    @Override   //Lidt langhåret syntaks for at kunne bruge den indbyggede clone funktion til at lave en kopi af beskeden.
+
+    @Override // Lidt langhåret syntaks for at kunne bruge den indbyggede clone funktion til
+              // at lave en kopi af beskeden.
     public IpMessage clone() {
-        try
-        {
+        try {
             return (IpMessage) super.clone();
-        }
-        catch (CloneNotSupportedException cnse)
-        {
+        } catch (CloneNotSupportedException cnse) {
             cnse.printStackTrace();
-            return this;            //hvis ikke muligt at clone så giv oprindelig besked tilbage.
+            return this; // hvis ikke muligt at clone så giv oprindelig besked tilbage.
         }
 
     }
